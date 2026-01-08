13b240dfdc7a83b62a5bb67c0b72a3331410349a
Nastsukii <lacb.lzn.luiz@gamil.com>
2026-01-08 16:57:11 -0300

    conectando com o banco de dados e testando

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import routes from './routes.js';
 const app = express();
-
+app.use(routes);
+app.use(express.json());
 export default app;
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,6 +1,22 @@
-import { Model } from "sequelize";
-
+import Sequelize, { Model } from 'sequelize';
 
 class User extends Model {
-    
+  static init(sequelize) {
+    Model.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        admin: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+        tableName: 'users',
+        underscored: true, 
+        timestamps: true, 
+      },
+    );
+  }
 }
+
+export default User;
