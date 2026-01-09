c206d430acb797ad935aa51f95e9635ded9bc581
Nastsukii <lacb.lzn.luiz@gamil.com>
2026-01-09 16:40:20 -0300

    fazendo as imagens aparecerem

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -2,16 +2,22 @@ import Sequelize, { Model } from 'sequelize';
 
 class Product extends Model {
   static init(sequelize) {
-    super.init(
+    Model.init(
       {
         name: Sequelize.STRING,
         price: Sequelize.INTEGER,
-        catergory: Sequelize.STRING,
+        category: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3001/product-file/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
-        tableName: 'product',
+        tableName: 'products',
         underscored: true,
         timestamps: true,
       },
