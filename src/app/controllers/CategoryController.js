587a596324627900a00e1607d05aa448463c86ce
Nastsukii <lacb.lzn.luiz@gamil.com>
2026-01-15 14:09:35 -0300

    Upload de imagens e autalizaçao de categories

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -14,7 +14,9 @@ class CategoryController {
       console.log(err);
       return response.status(400).json({ error: err.errors });
     }
+
     const { name } = request.body;
+    const { filename } = request.file;
 
     const existingCategory = await Category.findOne({
       where: {
@@ -28,10 +30,57 @@ class CategoryController {
 
     const newCategory = await Category.create({
       name,
+      path: filename,
     });
 
     return response.status(201).json(newCategory);
   }
+
+  async update(request, response) {
+    const schema = Yup.object({
+      name: Yup.string(),
+    });
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      console.log(err);
+      return response.status(400).json({ error: err.errors });
+    }
+
+    const { name } = request.body;
+    const { id } = request.params;
+
+    let path;
+    if (request.file) {
+      const { filename } = request.file;
+      path = filename;
+    }
+
+    const existingCategory = await Category.findOne({
+      where: {
+        name,
+      },
+    });
+
+    if (existingCategory) {
+      return response.status(400).json({ error: 'Category alredy exists' });
+    }
+    await Category.update(
+      {
+        name,
+        path,
+      },
+      {
+        where: {
+          id,
+        },
+      },
+    );
+
+    return response.status(201).json();
+  }
+
   async index(__request, response) {
     const categories = await Category.findAll();
 
--- src/routes.js
+++ src/routes.js
@@ -32,7 +32,19 @@ routes.put(
 );
 routes.get('/products', ProductController.index);
 
-routes.post('/categories', adminMiddleware, CategoryController.store);
+routes.post(
+  '/categories',
+  adminMiddleware,
+  upload.single('file'),
+  CategoryController.store,
+);
+
+routes.put(
+  '/categories/:id',
+  adminMiddleware,
+  upload.single('file'),
+  CategoryController.update,
+);
 routes.get('/categories', CategoryController.index);
 
 export default routes;
